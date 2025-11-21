06f805c142259c209adb686dda247a0ccb63cdf6
hastla007 <nowdoo.haderer@gmail.com>
2025-11-21 12:40:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-from .videourlhelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import folder_paths
-from .videourlhelpersuite.server import server
-from .videourlhelpersuite import documentation
-from .videourlhelpersuite import latent_preview
+from .videohelpersuite.server import server
+from .videohelpersuite import documentation
+from .videohelpersuite import latent_preview
 
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
